9ae2ee017f856c99f51679c7bca9b712e9653317
ksaisuai <skanokkorn7@gmail.com>
2025-06-19 02:50:51 +0000

    10

---
+++ cell05/ex10/ parameter_matching.py
@@ -0,0 +1,13 @@
+def main():
+    if len(sys.argv) != 2:
+        print("none")
+        return
+
+    param = sys.argv[1]
+    answer = input("What was the parameter? ")
+
+    if answer == param:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
+main()
