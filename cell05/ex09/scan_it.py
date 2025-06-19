cb95b8b1ce7dd2018c4d9c41eea5b794e645f4c9
ksaisuai <skanokkorn7@gmail.com>
2025-06-19 02:48:37 +0000

    09

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,14 @@
+def main():
+    if len(sys.argv) != 3:
+        print("none")
+        return
+
+    keyword = sys.argv[1]
+    text = sys.argv[2]
+
+    count = text.count(keyword)
+    if count == 0:
+        print("none")
+    else:
+        print(count)
+main()
