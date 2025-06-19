cdcfc60c2167c7edc080a8a9aa7f3d856e1a158d
ksaisuai <skanokkorn7@gmail.com>
2025-06-19 10:52:10 +0000

    01

--- cell07/ex01/family_affairs.py
+++ cell07/ex01/family_affairs.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python3
 
-def find_the_redheads(family_dict):
-    return list(filter(lambda name: family_dict[name] == "red", family_dict))
+def upcase_it(text):
+    return text.upper()
 
-dupont_family = {
-    "florian": "red",
-    "marie": "blond",
-    "virginie": "brunette",
-    "david": "red",
-    "franck": "red"
-}
-
-print(find_the_redheads(dupont_family))
+print(upcase_it("hello"))
