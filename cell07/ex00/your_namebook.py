97ffbaec79a190f2802142f5c523a20de747d8e6
ksaisuai <skanokkorn7@gmail.com>
2025-06-19 08:24:28 +0000

    00

---
+++ cell07/ex00/your_namebook.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def array_of_names(name_dict):
+    full_names = []
+    for first, last in name_dict.items():
+        full_name = f"{first.capitalize()} {last.capitalize()}"
+        full_names.append(full_name)
+    return full_names
+    
