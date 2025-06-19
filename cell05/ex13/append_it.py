40097e2c07299d83191c9235384b807f01e108c8
ksaisuai <skanokkorn7@gmail.com>
2025-06-19 03:00:37 +0000

    13

---
+++ cell05/ex13/append_it.py
@@ -0,0 +1,11 @@
+def main():
+    params = sys.argv[1:]
+    if len(params) == 0:
+        print("none")
+        return
+
+    for p in params:
+        if p.endswith("ism"):
+            continue
+        print(p + "ism")
+main()
